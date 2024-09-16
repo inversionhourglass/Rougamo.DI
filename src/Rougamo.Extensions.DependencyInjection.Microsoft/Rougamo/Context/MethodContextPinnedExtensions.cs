bf145464b798461d666c36fcd44c8093f7a1872c
ihourglass <inversionhourglass@outlook.com>
2024-09-16 08:40:18 +0800

    Add a new project Rougamo.Extnsions.DependencyInjection.Microsoft

---
+++ src/Rougamo.Extensions.DependencyInjection.Microsoft/Rougamo/Context/MethodContextPinnedExtensions.cs
@@ -0,0 +1,26 @@
+﻿using DependencyInjection.StaticAccessor;
+using System;
+
+namespace Rougamo.Context
+{
+    /// <summary>
+    /// </summary>
+    public static class MethodContextPinnedExtensions
+    {
+        /// <summary>
+        /// Get the root <see cref="IServiceProvider"/>
+        /// </summary>
+        public static IServiceProvider? GetRootServiceProvider(this MethodContext context)
+        {
+            return PinnedScope.RootServices;
+        }
+
+        /// <summary>
+        /// Get the current scope <see cref="IServiceProvider"/>, return root <see cref="IServiceProvider"/> if it is not within a scope.
+        /// </summary>
+        public static IServiceProvider? GetServiceProvider(this MethodContext context)
+        {
+            return PinnedScope.ScopedServices;
+        }
+    }
+}
